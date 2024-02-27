22d46d5cd488e65e2ea37ae01be8aacaf1cd3871
AHHHM <AHHHM@AHMLB>
2024-02-27 18:49:54 +0330

    AddHasBlockList

---
+++ Twitter.Clone.Settings/Features/BlockedList/GetIsBlockedByUserIds/GetIsBlockedByUserIdsQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Twitter.Clone.Settings.Infrastructure;
+
+namespace Twitter.Clone.Settings.Features.BlockedList.GetBlockedUsersByUserId
+{
+    public class GetIsBlockedByUserIdsQueryHandler : IRequestHandler<GetIsBlockedByUserIdsQuery, bool>
+    {
+        private readonly SettingsDbContext _dbContext;
+
+        public GetIsBlockedByUserIdsQueryHandler(SettingsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        public async Task<bool> Handle(GetIsBlockedByUserIdsQuery request, CancellationToken cancellationToken)
+        {
+            foreach (var item in request.UserIds)
+            {
+                int count =  _dbContext.BlockedUsers.TakeWhile(x => x.UserId == item && request.UserIds.Contains( x.BlockedUserId)).Count();
+                if (count > 1) return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Twitter.Clone.Settings/Features/BlockedList/GetIsBlockedByUserIds/GetIsBlockedByUserIdsResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Twitter.Clone.Settings.Features.BlockedList.GetBlockedUsersByUserId;
+
+public class GetIsBlockedByUserIdsResponse
+{
+    public Guid BlockedUserId { get; set; }
+
+}
+
+
+
